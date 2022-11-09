fef2f433728ce223bdabf9fdad3c16d96a0de678
Focus <maniaclord@bk.ru>
2022-11-09 18:04:02 +0300

    MPI Sem 10 working

--- MPI/main.cpp
+++ MPI/main.cpp
@@ -12,14 +12,19 @@ using namespace Seminars;
 //}
 
 int main(int *argc, char **argv) {
-    int numtask, rank;
-
+    int rank;
+    MPI_Status status;
     MPI_Init(argc, &argv);
-
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numtask);
 
-    printf("Hello", rank, numtask);
-
+    int a[5];
+    a[0] = 10;
+    a[1] = rank;
+    if (rank == 1)
+        MPI_Send(&a[1], 1, MPI_INT, 0, 99, MPI_COMM_WORLD);
+    else {
+        MPI_Recv(&a[0], 1, MPI_INT, 1, 99, MPI_COMM_WORLD, &status);
+        printf("%i", a[0]);
+    }
     MPI_Finalize();
 }
